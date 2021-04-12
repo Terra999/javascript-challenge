a41ea57d72461545e90cb83adc838d3d93321cb4
Terra Vaughn <vterra28@yahoo.com>
2021-04-11 20:08:08 -0500

    Got table to load. Working on filter for the form.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,6 +3,23 @@ console.log("app.js loaded!")
 // from data.js
 var tableData = data;
 
+// console.log(data);
+
+var tbody = d3.select("tbody");
+
+data.forEach(ufoData => {
+  console.log(ufoData);
+
+  var row = tbody.append("tr");
+
+  Object.entries(ufoData).forEach(([key, value]) => {
+
+    var cell = row.append("td");
+    cell.text(value);
+  
+  });
+});
+
 // Select the button
 var button = d3.select("#button");
 
@@ -11,7 +28,7 @@ var form = d3.select("#form");
 
 // Create event handlers 
 button.on("click", runEnter);
-form.on("submit",runEnter);
+form.on("submit", runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -26,6 +43,10 @@ function runEnter() {
   var inputValue = inputElement.property("value");
 
   console.log(inputValue);
-  console.log(tableData)
-};
+  console.log(tableData);
 
+var filterData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+console.log(filterData);
+
+};
