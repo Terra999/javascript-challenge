21ab8b3c86f233328d666a7f83b638b0ae6c7880
Terra Vaughn <vterra28@yahoo.com>
2021-04-13 15:47:27 -0500

    Working on code to clear and repopulate table.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,21 @@
+// Make sure app.js loaded
 console.log("app.js loaded!")
 
 // Assign the data from data.js to a descriptive variable
 var ufoData = data;
+ 
+// Make sure the data loaded.
 
-// console.log(data);
+console.log(data);
 
+// Get a hold of the tbody element in the DOM.
 var tbody = d3.select("tbody");
 
-data.forEach(ufoData => {
+console.log(ufoData);
 
-  console.log(ufoData);
+// Use d3 to append one table row `tr` for each ufo report object
+
+data.forEach(ufoData => {
 
   var row = tbody.append("tr");
 
@@ -17,42 +23,74 @@ data.forEach(ufoData => {
 
     var cell = row.append("td");
     cell.text(value);
-  
   });
 });
 
-// Select the button
-var button = d3.select("#filter-btn");
+    // Select the button
+    var button = d3.select("#filter-btn");
 
-// Select the form
-var form = d3.select("#form");
+    // Select the form
+    var form = d3.select("#form");
 
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit", runEnter);
+    // Create event handlers 
+    button.on("click", runEnter);
+    form.on("submit", runEnter);
 
-// Complete the event handler function for the form
-function runEnter() {
+    // Complete the event handler function for the form
+    function runEnter() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+      // Prevent the page from refreshing
+      d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+      // Select the input element and get the raw HTML node
+      var inputElement = d3.select("#datetime");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+      // Get the value property of the input element
+      var inputValue = inputElement.property("value");
 
-  // console.log(inputElement);
+      // console.log(inputElement);
 
-  // Filter for the date entered in the input field
+      // Filter for the date entered in the input field
 
-  var filterData = ufoData.filter(ufo => (ufo.datetime === inputValue));
+      var filterData = ufoData.filter(ufo => (ufo.datetime === inputValue));
 
-  console.log(filterData);
+      console.log(filterData);
 
-  // var list = d3.select("tr");
+      // var list = d3.select("tr");
 
-  // list.append("td").text(filterData);
+      // list.append("td").text(filterData);
 
-};
+    };
+
+    // // Complete the event handler function for the form
+    // function runEnter() {
+
+    //   // Prevent the page from refreshing
+    //   d3.event.preventDefault();
+
+    //   // Select the input element and get the raw HTML node
+    //   var inputElement = d3.select("#datetime");
+
+    //   // Get the value property of the input element
+    //   var inputValue = inputElement.property("value");
+
+    //   console.log(inputElement);
+
+    //   // Filter for the date entered in the input field
+    //     var filterData = ufoData.filter(ufo => (ufo.datetime === inputValue));
+
+    //     console.log(filterData);
+
+    //     data.filter(filterData => {
+
+    //       var row = tbody.append("tr");
+    
+    //         Object.entries(filterData).forEach(([key, value]) => {
+    
+    //         var cell = row.append("td");
+    //         cell.text(value);
+     
+    //         console.log(filterData);
+        //     });
+        // });
+      // };
